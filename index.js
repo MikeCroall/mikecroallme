e1f29423819b6d8694e5db576e59d884eed2aabf
mikecroall <mikebcroall@gmail.com>
2017-05-07 21:02:17 +0100

    App listens

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ var app = express();
 app.get("/", function(res, req) {
     res.redirect("http://johnjennings.net");
 });
+
+const portNumber = process.env.port || 8080;
+app.listen(portNumber, function() {
+    console.log("Server started on port " + portNumber);
+});
