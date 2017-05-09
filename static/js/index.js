4212ed9040fb60ff87713ce0eeee12505702b9b8
mikecroall <mikebcroall@gmail.com>
2017-05-09 23:50:37 +0100

    Smooth background

--- static/js/index.js
+++ static/js/index.js
@@ -1,9 +1,42 @@
 $(document).ready(function() {
     var movingBackground = $("#chkBackground").prop("checked");
 
+    var targetX = 50;
+    var targetY = 50;
+    var currentX = 50;
+    var currentY = 50;
+
+    function approachTarget() {
+        var xDiff = targetX - currentX;
+        var yDiff = targetY - currentY;
+
+        if (xDiff > 0.5 || xDiff < -0.5) {
+            currentX += 0.1 * xDiff;
+        }
+        if (yDiff > 0.5 || yDiff < -0.5) {
+            currentY += 0.1 * yDiff;
+        }
+
+        $("body").css("background-position", currentX + "% " + currentY + "%");
+
+        if (movingBackground) {
+            window.requestAnimationFrame(approachTarget);
+        }
+    }
+
+    if (movingBackground) {
+        window.requestAnimationFrame(approachTarget);
+    }
+
     $("#chkBackground").change(function() {
         movingBackground = this.checked;
-        if (!this.checked) {
+        if (this.checked) {
+            window.requestAnimationFrame(approachTarget);
+        } else {
+            targetX = 50;
+            targetY = 50;
+            currentX = 50;
+            currentY = 50;
             $("body").css("background-position", "50% 50%");
         }
     });
@@ -13,9 +46,8 @@ $(document).ready(function() {
             // 50 - default image position as percentage
             // 0.5 - to convert 0 to 1, to 0.5 to -0.5
             // 10 - multiplied by above range (0.5 to -0.5) to be offset from default image position percentage
-            var cssPosX = 50 - ((0.5 - (event.pageX / $(document).innerWidth())) * 10);
-            var cssPosY = 50 - ((0.5 - (event.pageY / $(document).innerHeight())) * 10);
-            $("body").css("background-position", cssPosX + "% " + cssPosY + "%");
+            targetX = 50 - ((0.5 - (event.pageX / $(document).innerWidth())) * 10);
+            targetY = 50 - ((0.5 - (event.pageY / $(document).innerHeight())) * 10);
         }
     });
 });
