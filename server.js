147e7ae138709bf2415e7b6368aef474fc70d61f
mikecroall <mikebcroall@gmail.com>
2017-05-14 15:04:40 +0100

    Stats saved on redirect link clicks

--- server.js
+++ server.js
@@ -6,12 +6,14 @@ var mongodb = require("mongodb");
 // Create server
 var app = express();
 
-// Connect to database
+// Setup database variables
 var mongoClient = mongodb.MongoClient;
 const dbuser = process.env.dbuser;
 const dbpass = process.env.dbpass;
 const dburi = "mongodb://" + dbuser + ":" + dbpass + "@ds137121.mlab.com:37121/mikecroallmestats";
 var objectID = mongodb.ObjectID;
+
+// Connect to database and save connection
 var db;
 mongoClient.connect(dburi, function(err, database_object) {
     if (err) {
@@ -21,6 +23,32 @@ mongoClient.connect(dburi, function(err, database_object) {
     }
 });
 
+// Stats increment
+function incrementStatByOne(linkName) {
+    if(db) {
+        db.collection("stats").findOne({
+            type: "main"
+        }, function(err, document) {
+            if(err) {
+                console.log("Loading stats failed", err);
+            } else {
+                if(document[linkName]) {
+                    document[linkName] += 1;
+                } else {
+                    document[linkName] = 1;
+                }
+                db.collection("stats").save(document, function(err, results) {
+                    if (err) {
+                        console.log("Saving stats failed", err);
+                    }
+                });
+            }
+        });
+    } else {
+        console.log("No database connection, stats not updated");
+    }
+}
+
 // Enable compression for smaller network transfer
 app.use(compression());
 
@@ -29,33 +57,19 @@ app.use("/", express.static("static"));
 
 // LinkedIn redirect
 app.get("/linkedin", function(req, res) {
+    incrementStatByOne("linkedinClicks");
     res.redirect("https://www.linkedin.com/in/mike-croall/");
 });
 
 // GitHub redirect
 app.get("/github", function(req, res) {
-
-    db.collection("stats").findOne({
-        type: "main"
-    }, function(err, document) {
-        if(err) {
-            console.log("Loading stats failed", err);
-        } else {
-            console.log("GOT STATS DOC", document);
-            // TODO increment document
-            db.collection("stats").save(document, function(err, results) {
-                if (err) {
-                    console.log("Saving stats failed", err);
-                }
-            });
-        }
-    });
-
+    incrementStatByOne("githubClicks");
     res.redirect("https://www.github.com/MikeCroall");
 });
 
 //Flickr redirect
 app.get("/flickr", function(req, res) {
+    incrementStatByOne("flickrClicks");
     res.redirect("https://www.flickr.com/photos/mcroall");
 });
 
