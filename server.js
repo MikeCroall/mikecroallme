c5ca2868eec867e6bacaa46c4ed8f5fdb2b25180
mikecroall <mikebcroall@gmail.com>
2017-05-16 13:30:00 +0100

    Debugging

--- server.js
+++ server.js
@@ -94,6 +94,12 @@ app.get("/admin", function(req, res) {
     var credentials = auth(req);
 
     if (!adminuser || !adminpass || !credentials || credentials.name !== adminuser || credentials.pass !== adminuser) {
+        if(!adminuser || !adminpass) {
+            console.log("process.env.adminuser or adminpass didn't exist");
+        }
+        if(!credentials) {
+            console.log("credentials didn't exist");
+        }
         res.redirect("/");
     } else {
         if(db) {
